02e981265060411e10bef4c38a8e95e93232ba21
hh35 <39907802+OvarNoot@users.noreply.github.com>
2023-11-05 19:13:50 -0500

    Efficiency Graph Reactive

--- app.R
+++ app.R
@@ -24,6 +24,7 @@ agg_eta_pfu_df <- pins::pin_read(board = pinboard, name = "agg_eta_pfu", version
 psut_df <- pins::pin_read(board = pinboard, name = "psut", version = "20230915T185731Z-c48a0")
 
 
+
 page1 <- tabPanel(
   title = "Global Map",
   titlePanel("Interactive Map"),
@@ -31,9 +32,9 @@ page1 <- tabPanel(
     sidebarPanel(
       selectInput("country", "Select a Country", 
                   choices = psut_df["Country"],
-                  selected = "WRLD"),
+                  selected = psut_df["Country"]),
       selectInput("year", "Select a Year", 
-                  choices = psut_df["Year"], selected = 2005)
+                  choices = psut_df["Year"], selected = psut_df["Year"])
     
     ),
     mainPanel(
@@ -50,11 +51,23 @@ page2 <- tabPanel(
   tags$h1("Efficiency Graph"),
   # Sidebar with a slider input for number of bins
     # Show a plot of the generated distribution
+  sidebarLayout(
+    sidebarPanel(
+      fluidRow(
+        column(3, h4("Energy Type"), radioButtons("energy_type", "", choices = c("E", "X"))),
+        column(3, h4("Product Aggregation"), radioButtons("product_aggregation", "", choices = c("Specified", "Despecified", "Grouped"))),
+        column(3, h4("Industry Aggregation"), radioButtons("industry_aggregation", "", choices = c("Specified", "Despecified", "Grouped"))),
+        column(3, h4("IEAMW"), radioButtons("ieamw", "", choices = c("IEA", "MW", "Both"))),
+        column(3, h4("Gross/Net"), radioButtons("gross_net", "", choices = c("Gross", "Net")))
+    )
+    ),
     mainPanel(
       plotOutput("Plot"),
-      tags$h1("Sankey Diagram"),
-      htmlOutput("sankeyPlot", inline = FALSE)
+      tags$h1("Sankey Diagram"), 
+      htmlOutput("sankeyPlot", inline = FALSE),
+      textOutput("gggg")
   )
+)
 )
 
 ui <- navbarPage(
@@ -99,6 +112,46 @@ server <- function(input, output) {
     return(Y_ago_1971)
   })
   
+  
+  eff1 <- reactive({
+    effi1 <- input$energy_type
+    return(effi1)
+  })
+  
+  eff2 <- reactive({
+    effi2 <- input$product_aggregation
+    return(effi2)
+  })
+  
+  eff3 <- reactive({
+    effi3 <- input$industry_aggregation
+    return(effi3)
+  })
+  
+  eff4 <- reactive({
+    effi4 <- input$ieamw
+    return(effi4)
+  })
+  
+  eff5 <- reactive({
+    effi5 <- input$gross_net
+    return(effi5)
+  })
+  
+  eff6 <- reactive({
+    effi6 <- input$country
+    return(effi6)
+  })
+  
+  eff7 <- reactive({
+    agg_eta_pfu_df1 <- agg_eta_pfu_df |> 
+      dplyr::filter(Country == input$country, IEAMW == input$ieamw, Energy.type == input$energy_type, 
+                    Product.aggregation == input$product_aggregation, 
+                    Industry.aggregation == input$industry_aggregation, 
+                    GrossNet == input$gross_net)
+    return(agg_eta_pfu_df1)
+  })
+  
   output$map <- renderLeaflet({
     leaflet() %>% 
       addTiles() %>%
@@ -107,16 +160,17 @@ server <- function(input, output) {
   
   output$Plot <- renderPlot({
     
-    agg_eta_pfu_df |> 
-      dplyr::filter(Country == "NGA", IEAMW == "Both", Energy.type == "E", 
-                    Product.aggregation == "Specified", 
-                    Industry.aggregation == "Specified", 
-                    GrossNet == "Gross") |> 
-      ggplot2::ggplot(mapping = ggplot2::aes(x = Year, y = eta_pu)) + 
-      ggplot2::geom_line()
+    # agg_eta_pfu_df |> 
+    #   dplyr::filter(Country == eff6(), IEAMW == eff4(), Energy.type == eff1(), 
+    #                 Product.aggregation == eff2(), 
+    #                 Industry.aggregation == eff3(), 
+    #                 GrossNet == eff5()) 
+    eff7() |> ggplot2::ggplot(mapping = ggplot2::aes(x = Year, y = eta_pu)) + ggplot2::geom_line()
   })
   
-  
+  output$gggg <- renderText({
+    c(eff6(),eff1(),eff2(),eff3(),eff4(),eff5())
+  })
 
   output$sankeyPlot <- renderUI({Recca::make_sankey(R = data1(),
                                                     U = data2(), 
@@ -130,3 +184,4 @@ server <- function(input, output) {
 
 # Run the application 
 shinyApp( ui,  server)
+
